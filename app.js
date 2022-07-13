f222266a0bc9bd40d1166c538c0f1e0202123052
Erik Ivanov <ivnverik@gmail.com>
2022-07-13 16:34:25 +0300

    Make healthbars dynamic

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ Vue.component('health-status', {
         id: {
             type: String,
             required: true
-        }
+        },
+        health: {
+            type: Number,
+            required: true
+        },
+    },
+    computed: {
+        healthbarWidth() {
+            return { width: (this.health > 0 ? this.health : 0) + '%' };
+        },
     },
     template: `
         <section :id="id" class="container">
             <slot></slot>
             <div class="healthbar">
-                <div class="healthbar-status"></div>
+                <div class="healthbar-status" :style="healthbarWidth"></div>
             </div>
         </section>
     `
