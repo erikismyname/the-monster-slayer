770308d5e360e0b5d1f4baae1bfccf6743cd33b3
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 19:49:22 +0300

    Add a battle log list item component

---
+++ src/components/BattleLogListItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li>{{ text }}</li>
+</template>
+
+<script>
+    export default {
+        props: {
+            text: {
+                type: String,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    li {
+        margin: 0.5rem 0;
+        list-style: none;
+    }
+
+    li:nth-child(even) {
+        background-color: black;
+        color: white;
+    }
+</style>
