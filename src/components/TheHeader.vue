a7dff53fd6511867cb23b92f941ba084b74e7a9a
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 19:00:48 +0300

    Extract some styles into the header component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <header id="main-header">
+    <header>
         <base-image :src="imagePath" :alt="imageAlt" />
         <h1>The Monster Slayer</h1>
         <base-image :src="imagePath" :alt="imageAlt" />
@@ -23,3 +23,17 @@
         },
     };
 </script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 1rem;
+        text-align: center;
+    }
+
+    h1 {
+        margin: 0 0.5rem;
+    }
+</style>
