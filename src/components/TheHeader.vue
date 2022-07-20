2c14baf769439f84e0548fc4926ecd7f8e0c7c49
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 15:16:48 +0300

    Extract header markup

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        <img src="./assets/fire-torch.jpg" alt="A burning fire torch" />
+        <h1>The Monster Slayer</h1>
+        <img src="./assets/fire-torch.jpg" alt="A burning fire torch" />
+    </header>
+</template>
