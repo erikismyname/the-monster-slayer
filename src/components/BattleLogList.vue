807ad1bd1ef302ca5be7de5c8ca3e27a3ba6d962
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 19:49:05 +0300

    Add a battle log list component

---
+++ src/components/BattleLogList.vue
@@ -0,0 +1,43 @@
+<template>
+    <transition-group tag="ul" name="entry">
+        <battle-log-list-item
+            v-for="entry in battleLog"
+            :key="entry.id"
+            :text="entry.text"
+        />
+    </transition-group>
+</template>
+
+<script>
+    import BattleLogListItem from "./BattleLogListItem.vue";
+
+    export default {
+        components: {
+            BattleLogListItem,
+        },
+        props: {
+            battleLog: {
+                type: Array,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    .entry-enter {
+        opacity: 0;
+    }
+
+    .entry-enter-active {
+        transition: opacity 1s linear;
+    }
+
+    .entry-enter-to {
+        opacity: 1;
+    }
+
+    .entry-move {
+        transition: transform 0.3s ease-out;
+    }
+</style>
