82cf67b540b7a616f9f0ff602a53468b8177db54
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 15:16:22 +0300

    Extract app logic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,169 @@
 <template></template>
 
 <script>
+    import getRandomValueBetween from "./util/getRandomValueBetween.js";
+    import formatEntry from "./util/formatEntry.js";
+
+    export default {
+        data() {
+            return {
+                isModalVisible: true,
+                monsterHealth: 100,
+                hasMonsterSecondWind: false,
+                hasMonsterUsedSecondWind: false,
+                playerName: "",
+                playerHealth: 100,
+                playerHealthPotions: 5,
+                hasPlayerSecondWind: false,
+                hasPlayerUsedSecondWind: false,
+                currentRound: 0,
+                winner: "",
+                battleLog: [],
+                battleLogEntriesOrder: "descending",
+            };
+        },
+        watch: {
+            monsterHealth(newHealth) {
+                if (newHealth < 0) {
+                    this.monsterHealth = 0;
+                }
+            },
+            playerHealth(newHealth) {
+                if (newHealth < 0) {
+                    this.playerHealth = 0;
+                }
+            },
+            battleLogEntriesOrder() {
+                const sortedBattleLog = [];
+
+                for (let i = 0; i < this.battleLog.length; i += 2) {
+                    sortedBattleLog.unshift(
+                        this.battleLog[i],
+                        this.battleLog[i + 1]
+                    );
+                }
+
+                this.battleLog = sortedBattleLog;
+            },
+        },
+        methods: {
+            setPlayerName(playerName) {
+                this.playerName = playerName;
+                this.isModalVisible = false;
+            },
+            attackMonster(isSpecialAttack) {
+                const playerAttackPoints = isSpecialAttack
+                    ? getRandomValueBetween(9, 14)
+                    : getRandomValueBetween(5, 10);
+                this.monsterHealth -= playerAttackPoints;
+
+                this.addEntryToBattleLog({
+                    contender: "Player",
+                    action: "attack",
+                    points: playerAttackPoints,
+                });
+
+                this.attackPlayer();
+            },
+            attackPlayer() {
+                const monsterAttackPoints = getRandomValueBetween(7, 12);
+                this.playerHealth -= monsterAttackPoints;
+
+                this.addEntryToBattleLog({
+                    contender: "Monster",
+                    action: "attack",
+                    points: monsterAttackPoints,
+                });
+
+                this.endCurrentRound();
+            },
+            healPlayer() {
+                const playerHealPoints = getRandomValueBetween(8, 11);
+                this.playerHealth += playerHealPoints;
+
+                if (this.playerHealth > 100) {
+                    this.playerHealth = 100;
+                }
+
+                this.playerHealthPotions--;
+
+                this.addEntryToBattleLog({
+                    contender: "Player",
+                    action: "heal",
+                    points: playerHealPoints,
+                });
+
+                this.attackPlayer();
+            },
+            endCurrentRound() {
+                if (this.monsterHealth <= 0 && this.playerHealth <= 0) {
+                    this.winner = "draw";
+                } else if (this.monsterHealth <= 0) {
+                    this.processMonsterDying();
+                } else if (this.playerHealth <= 0) {
+                    this.processPlayerDying();
+                }
+
+                this.currentRound++;
+            },
+            processPlayerDying() {
+                if (this.hasSecondWind("player")) {
+                    this.hasPlayerSecondWind = true;
+                    this.hasPlayerUsedSecondWind = true;
+                    this.playerHealth = 50;
+
+                    return;
+                }
+
+                this.winner = "monster";
+            },
+            processMonsterDying() {
+                if (this.hasSecondWind("monster")) {
+                    this.hasMonsterSecondWind = true;
+                    this.hasMonsterUsedSecondWind = true;
+                    this.monsterHealth = 50;
+
+                    return;
+                }
+
+                this.winner = "player";
+            },
+            hasSecondWind(contender) {
+                const hasContenderUsedSecondWind =
+                    contender === "player"
+                        ? this.hasPlayerUsedSecondWind
+                        : this.hasMonsterUsedSecondWind;
+
+                return !hasContenderUsedSecondWind && Math.random() > 0.5;
+            },
+            startNewGame() {
+                this.monsterHealth = 100;
+                this.hasMonsterSecondWind = false;
+                this.hasMonsterUsedSecondWind = false;
+
+                this.playerHealth = 100;
+                this.hasPlayerSecondWind = false;
+                this.hasPlayerUsedSecondWind = false;
+
+                this.currentRound = 0;
+                this.winner = "";
+                this.battleLog = [];
+            },
+            surrenderToMonster() {
+                this.winner = "monster";
+            },
+            addEntryToBattleLog(entry) {
+                const formattedEntry = formatEntry(entry);
+                this.battleLog.unshift(formattedEntry);
+            },
+            changeBattleLogEntriesOrder() {
+                this.battleLogEntriesOrder =
+                    this.battleLogEntriesOrder === "descending"
+                        ? "ascending"
+                        : "descending";
+            },
+        },
+    };
 </script>
 
 <style>
