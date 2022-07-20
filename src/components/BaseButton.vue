c0a042f7a9731b37f6aebf73bfee3185138d4c72
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 19:19:00 +0300

    Create a base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <button>
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {};
+</script>
+
+<style scoped>
+    button {
+        width: 9rem;
+        margin: 0.5rem 0;
+        padding: 1rem;
+        border: none;
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+    }
+
+    button:hover {
+        cursor: pointer;
+        color: black;
+        background-color: white;
+    }
+
+    button:disabled {
+        background-color: #b5b5b5;
+        color: #3f3f3f;
+        cursor: not-allowed;
+    }
+</style>
