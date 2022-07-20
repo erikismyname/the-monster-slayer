38589b75ec8f56a2093ebce36ca9085f94714ac0
Erik Ivanov <ivnverik@gmail.com>
2022-07-20 15:18:01 +0300

    Extract game over markup and logic

---
+++ src/components/GameOver.vue
@@ -0,0 +1,39 @@
+<template>
+    <section v-if="isGameOver" class="container">
+        <h2>Game Over</h2>
+        <h3 v-if="hasPlayerWon">You WON!</h3>
+        <h3 v-else-if="hasMonsterWon">You LOST!</h3>
+        <h3 v-else>DRAW!</h3>
+        <button @click="startNewGame">Start New Game</button>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: {
+            winner: {
+                type: String,
+                required: true,
+            },
+        },
+        computed: {
+            isGameOver() {
+                return this.winner;
+            },
+            hasPlayerWon() {
+                return this.winner === "player";
+            },
+            hasMonsterWon() {
+                return this.winner === "monster";
+            },
+        },
+        methods: {
+            startNewGame() {
+                this.$emit("start-new-game");
+            },
+        },
+    };
+</script>
+
+<style>
+</style>
