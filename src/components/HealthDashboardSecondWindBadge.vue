3b4d14603a8b84642fe8b8f8baea87c5f7e32db8
Erik Ivanov <ivnverik@gmail.com>
2022-07-21 15:33:39 +0300

    Extract second wind badge into a separate component

---
+++ src/components/HealthDashboardSecondWindBadge.vue
@@ -0,0 +1,27 @@
+<template>
+    <span v-if="hasContenderSecondWind" class="second-wind-badge">
+        Second Wind
+    </span>
+</template>
+
+<script>
+    export default {
+        props: {
+            hasContenderSecondWind: {
+                type: Boolean,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    .second-wind-badge {
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: rotateZ(45deg) translateX(2.2rem) translateY(-0.3rem);
+        width: 8rem;
+        padding: 0.1rem;
+    }
+</style>
