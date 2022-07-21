d677fd770c7459f6b3e84ea0ea3d2f86ea2bf18b
Erik Ivanov <ivnverik@gmail.com>
2022-07-21 15:08:29 +0300

    Remove an obsolete function's parameter

--- src/util/getRandomName.js
+++ src/util/getRandomName.js
@@ -1,3 +1,3 @@
-export default function getRandomName(gender) {
+export default function getRandomName() {
     return ['Arne', 'Bjorn', 'Harald', 'Leif', 'Torsten'][Math.floor(Math.random() * 5)];
 }
