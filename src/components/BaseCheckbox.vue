38cee72ef40b477453fec89d1cd61975d0704342
Erik Ivanov <ivnverik@gmail.com>
2022-07-21 13:59:36 +0300

    Add a base checkbox component

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,33 @@
+<template>
+    <input 
+        type="checkbox" 
+        :checked="checked" 
+        @change="updateCheckboxValue" 
+    />
+</template>
+
+<script>
+    export default {
+        model: {
+            prop: "checked",
+            event: "change",
+        },
+        props: {
+            checked: {
+                type: Boolean,
+                required: true,
+            },
+        },
+        methods: {
+            updateCheckboxValue(event) {
+                this.$emit("change", event.target.checked);
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    input {
+        margin-right: .3rem;
+    }
+</style>
