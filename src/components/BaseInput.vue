75bb2015b824a6b3df05b4fa25b602081fa1351e
Erik Ivanov <ivnverik@gmail.com>
2022-07-21 13:59:19 +0300

    Add a base input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <input type="text" :value="value" @input="updateInputValue" />
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                type: String,
+                required: true,
+            },
+        },
+        methods: {
+            updateInputValue(event) {
+                this.$emit("input", event.target.value);
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    input {
+        padding: 0.2rem;
+        font-family: inherit;
+        border: 0.1rem solid black;
+        width: 40%;
+    }
+
+    .invalid {
+        border-color: #880808;
+    }
+</style>
