06dc1ff0989da915df8c777fd9fb67f3fe425199
Erik Ivanov <ivnverik@gmail.com>
2022-07-25 14:53:25 +0300

    Modify existing test and add a new one

--- tests/unit/BattleLog/BattleLogListItem.spec.js
+++ tests/unit/BattleLog/BattleLogListItem.spec.js
@@ -3,13 +3,22 @@ import { mount } from '@vue/test-utils';
 import BattleLogListItem from '@/components/BattleLog/BattleLogListItem.vue';
 
 describe('BattleLogListItem.vue', () => {
-    it('should render text prop', () => {
-        const wrapper = mount(BattleLogListItem, {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = mount(BattleLogListItem, {
             propsData: {
                 text: 'Test'
             }
         });
+    });
 
+    it('should render exactly one li element', () => {
+        expect(wrapper.find('li').exists()).toBe(true);
+        expect(wrapper.findAll('li')).toHaveLength(1);
+    });
+
+    it('should render text prop', () => {
         expect(wrapper.text()).toBe('Test');
     });
 });
