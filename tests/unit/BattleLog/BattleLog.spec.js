1008ac601f27cbfe8f1c44554f56ec05f8028817
Erik Ivanov <ivnverik@gmail.com>
2022-07-25 15:47:29 +0300

    Rename tests and modify one of them

--- tests/unit/BattleLog/BattleLog.spec.js
+++ tests/unit/BattleLog/BattleLog.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 
 import BattleLog from '@/components/BattleLog/BattleLog.vue';
 
-describe.only('BattleLog.vue', () => {
+describe('BattleLog.vue', () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallowMount(BattleLog, {
+        wrapper = mount(BattleLog, {
             propsData: {
                 battleLog: [],
                 battleLogEntriesOrder: 'descending'
@@ -14,28 +14,30 @@ describe.only('BattleLog.vue', () => {
         });
     });
 
-    it('should render arrow down and not render arrow up when entries order is descending', () => {
+    it('should render an arrow down instead of an arrow up when entries order is descending', () => {
         expect(wrapper.find('.fa-arrow-down').exists()).toBe(true);
         expect(wrapper.find('.fa-arrow-up').exists()).toBe(false);
     });
 
-    it('should render arrow up and not render arrow down when entries order is ascending', async () => {
+    it('should render an arrow up instead of an arrow down when entries order is ascending', async () => {
         await wrapper.setProps({ battleLogEntriesOrder: 'ascending' });
 
         expect(wrapper.find('.fa-arrow-down').exists()).toBe(false);
         expect(wrapper.find('.fa-arrow-up').exists()).toBe(true);
     });
 
-    it('should emit an event when each one of the arrows is clicked', async () => {
+    it('should emit an event whenever each arrow is clicked', async () => {
         await wrapper.find('.fa-arrow-down').trigger('click');
 
-        expect(wrapper.emitted('change-battle-log-entries-order')).toBeTruthy();
-        expect(wrapper.emitted('change-battle-log-entries-order').length).toBe(1);
+        const emittedEvent = wrapper.emitted('change-battle-log-entries-order');
+
+        expect(emittedEvent).toBeTruthy();
+        expect(emittedEvent.length).toBe(1);
 
         await wrapper.setProps({ battleLogEntriesOrder: 'ascending' });
 
         await wrapper.find('.fa-arrow-up').trigger('click');
 
-        expect(wrapper.emitted('change-battle-log-entries-order').length).toBe(2);
+        expect(emittedEvent.length).toBe(2);
     });
 });
