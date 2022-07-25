04e789dcbc51de147fcc1a7529b2915c4b6aedf0
Erik Ivanov <ivnverik@gmail.com>
2022-07-25 19:26:25 +0300

    Add a test for base icon component

---
+++ tests/unit/common/BaseIcon.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+
+import BaseIcon from '@/components/common/BaseIcon.vue';
+
+describe('BaseIcon.vue', () => {
+    it('should render an icon', () => {
+        const wrapper = mount(BaseIcon);
+
+        expect(wrapper.find('i').exists()).toBe(true);
+    });
+});
