61277776ded210f952d9befbb1433b2df3215430
Erik Ivanov <ivnverik@gmail.com>
2022-07-26 23:24:16 +0300

    Extract modal header into a separate component

---
+++ src/components/Modal/ModalHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h2>Greetings, Warrior!</h2>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        padding: 0.5rem;
+        border-top-left-radius: 0.2rem;
+        border-top-right-radius: 0.2rem;
+    }
+</style>
