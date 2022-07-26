0d3c37bd8263223c4ae312c6a80ddac9c859e5ba
Erik Ivanov <ivnverik@gmail.com>
2022-07-26 23:23:52 +0300

    Extract modal backdrop into a separate component

---
+++ src/components/Modal/ModalBackdrop.vue
@@ -0,0 +1,20 @@
+<template>
+    <div 
+        class="backdrop" 
+        data-testid="backdrop"
+    >
+    </div>
+</template>
+
+<style scoped>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        background-color: black;
+        opacity: 0.5;
+        z-index: 1;
+    }
+</style>
