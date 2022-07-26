5f8c0ec8b895ac563efa238e2a3ae3833b8c6cc6
Erik Ivanov <ivnverik@gmail.com>
2022-07-26 10:40:55 +0300

    Modify modal component test file

--- tests/unit/Modal.spec.js
+++ tests/unit/Modal.spec.js
@@ -10,57 +10,61 @@ describe('Modal.vue', () => {
             propsData: {
                 isVisible: false
             }
-        })
+        });
     });
 
-    it('should not render backdrop when isVisible is false', () => {
+    it('should not render backdrop when isVisible prop is false', () => {
         expect(wrapper.find('[data-testid="backdrop"]').exists()).toBe(false);
     });
 
-    it('should render backdop when isVisible is true', async () => {
+    it('should render backdop when isVisible prop is true', async () => {
         await wrapper.setProps({ isVisible: true });
 
         expect(wrapper.find('[data-testid="backdrop"]').exists()).toBe(true);
     });
 
-    it('should not render modal when isVisible is false', () => {
+    it('should not render modal when isVisible prop is false', () => {
         expect(wrapper.find('[data-testid="modal"]').exists()).toBe(false);
     });
 
-    it('should render modal when isVisible is true', async () => {
+    it('should render modal when isVisible prop is true', async () => {
         await wrapper.setProps({ isVisible: true });
 
         expect(wrapper.find('[data-testid="modal"]').exists()).toBe(true);
     });
 
-    it('should not attach invalid class on initial render', async () => {
+    it('should not attach invalid input class on initial render', async () => {
         await wrapper.setProps({ isVisible: true });
-
-        expect(wrapper.classes('invalid')).toBe(false);
-    });
-
-    it('should attach invalid class when player name is empty', async () => {
-        await wrapper.setProps({ isVisible: true });
-
-        await wrapper.find('[data-testid="start-new-game-btn"]').trigger('click');
-
-        expect(wrapper.find('[data-testid="player-name"]').element._prevClass).toBe('invalid');
-
-        await wrapper.find('#generate-random-name').setChecked();
 
         expect(wrapper.find('[data-testid="player-name"]').element._prevClass).toBe('');
+    });
 
-        await wrapper.find('#generate-random-name').setChecked(false);
+    it('should attach invalid input class when player name is empty', async () => {
+        await wrapper.setProps({ isVisible: true });
 
-        expect(wrapper.find('[data-testid="player-name"]').element._prevClass).toBe('invalid');
+        await wrapper.find('[data-testid="start-game-btn"]').trigger('click');
+
+        const playerNameInput = wrapper.find('[data-testid="player-name"]');
+
+        const generateRandomNameCheckbox = wrapper.find('#generate-random-name');
+
+        expect(playerNameInput.element._prevClass).toBe('invalid');
+
+        await generateRandomNameCheckbox.setChecked();
+
+        expect(playerNameInput.element._prevClass).toBe('');
+
+        await generateRandomNameCheckbox.setChecked(false);
+
+        expect(playerNameInput.element._prevClass).toBe('invalid');
     });
 
     it('emits an event when start new game button is clicked', async () => {
         await wrapper.setProps({ isVisible: true });
 
-        await wrapper.find('#player-name').setValue('Test');
+        await wrapper.find('[data-testid="player-name"]').setValue('Test');
 
-        await wrapper.find('button').trigger('click');
+        await wrapper.find('[data-testid="start-game-btn"]').trigger('click');
 
         const emittedEvent = wrapper.emitted('set-player-name');
 
