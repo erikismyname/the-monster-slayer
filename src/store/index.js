b69b7bd5f250cc8f0de5015075d8de9dacc7a9bf
Erik Ivanov <ivnverik@gmail.com>
2022-07-28 15:43:25 +0300

    Delete obsolete code and restructure index store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-// order below
-import player from './modules/player.js';
-import monster from './modules/monster.js';
+
 import battleLog from './modules/battleLog.js';
 import game from './modules/game.js';
+import monster from './modules/monster.js';
+import player from './modules/player.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-    },
-    getters: {
-    },
-    mutations: {
-    },
-    actions: {
-    },
     modules: {
-        player,
-        monster,
         battleLog,
-        game
+        game,
+        monster,
+        player
     }
-})
+});
