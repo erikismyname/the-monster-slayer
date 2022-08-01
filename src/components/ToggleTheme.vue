4ffc96a7df2752ab50df98a2198cebe86f0d8bd4
Erik Ivanov <ivnverik@gmail.com>
2022-08-01 20:58:07 +0300

    Implement a toggle theme component

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,32 @@
+<template>
+    <transition>
+        <base-icon
+            v-if="isThemeLight"
+            @click.native="setTheme"
+            class="fa-solid fa-sun"
+        />
+        <base-icon 
+            v-else 
+            @click.native="setTheme" 
+            class="fa-solid fa-moon" 
+        />
+    </transition>
+</template>
+
+<script>
+    import { mapGetters, mapActions } from "vuex";
+
+    import BaseIcon from "./common/BaseIcon.vue";
+
+    export default {
+        components: {
+            BaseIcon,
+        },
+        computed: {
+            ...mapGetters(["isThemeLight"]),
+        },
+        methods: {
+            ...mapActions(["setTheme"]),
+        },
+    };
+</script>
