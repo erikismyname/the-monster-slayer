8fd22649453dea37bd3da1bd8338b7cbf2fc474d
Erik Ivanov <ivnverik@gmail.com>
2022-08-01 15:21:05 +0300

    Implement scroll top top component

---
+++ src/components/ScrollToTop.vue
@@ -0,0 +1,36 @@
+<template>
+    <base-icon
+        v-if="isScrollToTopIconVisible"
+        @click.native="scrollToTop"
+        class="fa-solid fa-circle-arrow-up"
+    />
+</template>
+
+<script>
+    import BaseIcon from "./common/BaseIcon.vue";
+
+    export default {
+        components: {
+            BaseIcon,
+        },
+        data() {
+            return { scrollY: 0 };
+        },
+        computed: {
+            isScrollToTopIconVisible() {
+                return this.scrollY > 300;
+            },
+        },
+        mounted() {
+            addEventListener("scroll", this.setScrollY);
+        },
+        methods: {
+            setScrollY() {
+                this.scrollY = scrollY;
+            },
+            scrollToTop() {
+                scrollTo({ top: 0, behavior: "smooth" });
+            },
+        },
+    };
+</script>
