5b99f532e33e7f1802e61d31637fbadab7c2ac55
Erik Ivanov <ivnverik@gmail.com>
2022-08-01 15:21:26 +0300

    Add a new base icon class

--- src/components/common/BaseIcon.vue
+++ src/components/common/BaseIcon.vue
@@ -10,4 +10,11 @@
     .fa-solid:hover {
         cursor: pointer;
     }
+
+    .fa-circle-arrow-up {
+        position: fixed;
+        right: 1rem;
+        bottom: 1rem;
+        font-size: 2rem;
+    }
 </style>
