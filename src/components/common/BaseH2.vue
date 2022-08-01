429ce94ec3689f4a18b0179c73f6a8e7a3b343a5
Erik Ivanov <ivnverik@gmail.com>
2022-08-01 15:55:33 +0300

    Create a base h2 component

---
+++ src/components/common/BaseH2.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>
+        <slot></slot>
+    </h2>
+</template>
+
+<style scoped>
+    h2 {
+        margin-bottom: 1rem;
+    }
+
+    .contender {
+        display: inline-block;
+        vertical-align: middle;
+        margin-right: 0.3rem;
+        margin-bottom: 0;
+    }
+</style>
