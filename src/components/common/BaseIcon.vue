c52fdad5d4613fd0f942346fe25ef41e6360388e
Erik Ivanov <ivnverik@gmail.com>
2022-08-02 15:19:45 +0300

    Add more styles and base icon component

--- src/components/common/BaseIcon.vue
+++ src/components/common/BaseIcon.vue
@@ -16,13 +16,24 @@
         right: 1rem;
         bottom: 1rem;
         font-size: 2rem;
+        border-radius: 50%;
+        background-color: white;
+    }
+
+    .fa-circle-arrow-up.dark {
+        background-color: black;
+        color: white;
+    }
+
+    .fa-circle-arrow-up:hover {
+        transform: scale(1.1);
     }
 
     .fa-sun,
     .fa-moon {
         position: absolute;
         right: 3rem;
-        padding: .3rem;
+        padding: 0.3rem;
         border-radius: 50%;
     }
 
@@ -30,5 +41,34 @@
     .fa-moon:hover {
         background-color: white;
         color: black;
+    }
+
+    @media (max-width: 28.12rem) {
+        .fa-sun,
+        .fa-moon {
+            right: 1rem;
+        }
+    }
+
+    @media (max-width: 24.37rem) {
+        .fa-sun,
+        .fa-moon {
+            right: 0;
+        }
+    }
+
+    @media (max-width: 21.56rem) {
+        .fa-sun,
+        .fa-moon {
+            right: 0.8rem;
+            top: 5.5rem;
+        }
+    }
+
+    @media (max-width: 16.93rem) {
+        .fa-sun,
+        .fa-moon {
+            top: 7.8rem;
+        }
     }
 </style>
