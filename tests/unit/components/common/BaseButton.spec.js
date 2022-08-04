af2d35c3a3a5226048e79d3abfec6ef5404f545a
Erik Ivanov <ivnverik@gmail.com>
2022-08-04 13:08:00 +0300

    Add tests for base button

---
+++ tests/unit/components/common/BaseButton.spec.js
@@ -0,0 +1,23 @@
+import { mount } from '@vue/test-utils';
+
+import BaseButton from '@/components/common/BaseButton';
+
+describe('BaseButton.vue', () => {
+    let wrapper;
+    let button;
+
+    beforeEach(() => {
+        wrapper = mount(BaseButton, {
+            slots: { default: 'Test' }
+        });
+        button = wrapper.find('button');
+    });
+
+    it('should render a button', () => {
+        expect(button.exists()).toBe(true);
+    });
+
+    it('should render a correct button label', () => {
+        expect(button.text()).toBe('Test');
+    })
+});
