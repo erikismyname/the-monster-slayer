2f732f3441b10708fd81a7f93d2be5d4640d60aa
Erik Ivanov <ivnverik@gmail.com>
2022-08-05 20:08:52 +0300

    Modify contender dashboard

--- src/components/ContendersDashboard/ContendersDashboard.vue
+++ src/components/ContendersDashboard/ContendersDashboard.vue
@@ -1,26 +1,16 @@
 <template>
     <div>
-        <contenders-dashboard-card contender="monster">
-            <base-h2 class="contender">Monster</base-h2>
-        </contenders-dashboard-card>
-
-        <contenders-dashboard-card contender="player">
-            <base-h2 class="contender">{{ name }}</base-h2>
-        </contenders-dashboard-card>
+        <contenders-dashboard-card contender="monster" />
+        <contenders-dashboard-card contender="player" />
     </div>
 </template>
 
 <script>
-    import { mapGetters } from "vuex";
-
-    import BaseH2 from "@/components/common/BaseH2";
     import ContendersDashboardCard from "@/components/ContendersDashboard/ContendersDashboardCard";
 
     export default {
         components: {
-            BaseH2,
             ContendersDashboardCard,
         },
-        computed: mapGetters("player", ["name"]),
     };
 </script>
