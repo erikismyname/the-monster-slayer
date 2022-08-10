92482a0f22ad37bcea931b963d09877df289e550
Erik Ivanov <ivnverik@gmail.com>
2022-08-10 21:22:15 +0300

    Use ternary instead if else in format entry function

--- src/utils/formatEntry.js
+++ src/utils/formatEntry.js
@@ -1,11 +1,7 @@
 export default function formatEntry({ contender, action, points }) {
-    let formattedEntry;
-
-    if (action === 'attack' || action === 'specialAttack') {
-        formattedEntry = `${contender} attacked and ${(points ? `dealt ${points} damage` : 'missed')}.`;
-    } else {
-        formattedEntry = `${contender} ${(points ? `healed for ${points} points` : 'dropped his potion and couldn\'t heal')}.`;
-    }
+    const formattedEntry = action === 'heal'
+        ? `${contender} ${(points ? `healed for ${points} points` : 'dropped his potion and couldn\'t heal')}.`
+        : `${contender} attacked and ${(points ? `dealt ${points} damage` : 'missed')}.`;
 
     return {
         id: generateEntryId(),
