b9d8c74f967208073aa25c4a3f8bf45fd9eb6b3b
Erik Ivanov <ivnverik@gmail.com>
2022-08-10 17:02:51 +0300

    Modify contenders dashboard card tests

--- tests/unit/components/ContendersDashboard/ContendersDashboardCard.spec.js
+++ tests/unit/components/ContendersDashboard/ContendersDashboardCard.spec.js
@@ -1,37 +1,49 @@
-import { shallowMount } from '@vue/test-utils';
-
+import createWrapper from '@/utils/setupTests';
 import ContendersDashboardCard from '@/components/ContendersDashboard/ContendersDashboardCard';
 
 describe('CotendersDashboardCard.vue', () => {
+    let monster;
+    let player;
+    let game;
+    let modules;
+    let propsData;
+
+    beforeEach(() => {
+        monster = createMonsterModule();
+        player = createPlayerModule();
+        game = createGameModule();
+        modules = { monster, player, game };
+        propsData = { contender: 'monster' };
+    });
+
     it('should not apply dark class when dark mode is off', () => {
-        const wrapper = createWrapper();
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.classes('dark')).toBe(false);
     });
 
     it('should apply dark class when dark mode is on', () => {
-        computed.isDarkModeOn = () => true;
-        const wrapper = createWrapper();
+        game.getters.isDarkModeOn = () => true;
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.classes('dark')).toBe(true);
     });
 
     it('should render correct name when contender is monster', () => {
-        const wrapper = createWrapper();
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.find('[data-testid="contender"]').text()).toBe('Monster')
     });
 
     it('should render correct name when contender is player', () => {
         propsData.contender = 'player';
-        const wrapper = createWrapper();
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.find('[data-testid="contender"]').text()).toBe('Test');
     });
 
     it('should render monster avatar and not player icons when contender is monster', () => {
-        propsData.contender = 'monster';
-        const wrapper = createWrapper();
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.find('[data-testid="monster-avatar"]').exists()).toBe(true);
         expect(wrapper.find('[data-testid="player-icons"]').exists()).toBe(false);
@@ -39,7 +51,7 @@ describe('CotendersDashboardCard.vue', () => {
 
     it('should render player icons and not monster avatar when contender is player', () => {
         propsData.contender = 'player'
-        const wrapper = createWrapper();
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.find('[data-testid="monster-avatar"]').exists()).toBe(false);
         expect(wrapper.find('[data-testid="player-icons"]').exists()).toBe(true);
@@ -47,27 +59,39 @@ describe('CotendersDashboardCard.vue', () => {
 
     it('should render player health potions number', () => {
         propsData.contender = 'player';
-        const wrapper = createWrapper();
+        const wrapper = createWrapper(ContendersDashboardCard, modules, { propsData });
 
         expect(wrapper.find('[data-testid="health-potions-counter"]').text()).toBe('5');
     });
 });
 
-const propsData = { contender: 'monster' };
+function createMonsterModule() {
+    return {
+        namespaced: true,
+        getters: {
+            hasSecondWind: () => false,
+            health: () => 100
+        }
+    }
+}
 
-const computed = {
-    hasMonsterSecondWind: () => false,
-    monsterHealth: () => 100,
-    hasPlayerSecondWind: () => false,
-    playerHealth: () => 100,
-    playerHealthPotions: () => 5,
-    playerName: () => 'Test',
-    isDarkModeOn: () => false
-};
+function createPlayerModule() {
+    return {
+        namespaced: true,
+        getters: {
+            hasSecondWind: () => false,
+            health: () => 100,
+            healthPotions: () => 5,
+            name: () => 'Test'
+        }
+    }
+}
 
-function createWrapper() {
-    return shallowMount(ContendersDashboardCard, {
-        propsData,
-        computed
-    });
+function createGameModule() {
+    return {
+        namespaced: true,
+        getters: {
+            isDarkModeOn: () => false
+        }
+    }
 }
